0662c472c3b9bfddd568516d846772f78007b593
Ama Freeman <ama.freeman96@gmail.com>
2020-11-08 21:06:11 -0500

    Add graphics and code for enemy spawn

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return startingPosition + GetRandomDirection() * Random.Range(10f, 70f);
+    }
+
+    //Generate Random normalized direction
+    public static Vector3 GetRandomDirection()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1f, -1f), UnityEngine.Random.Range(-1f, -1f)).normalized;
+    }
+}
